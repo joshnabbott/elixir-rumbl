f1f005ee96ba914b737c3adf79192edecafabb2c
Joshua Abbott <joshnabbott@gmail.com>
2016-08-14 10:32:05 -0600

    Initial commit

---
+++ lib/rumbl/repo.ex
@@ -0,0 +1,24 @@
+defmodule Rumbl.Repo do
+  @moduledoc """
+  In memory repository
+  """
+  def all(Rumbl.User) do
+    [
+      %Rumbl.User{id: "1", name: "Joshua Abbott", username: "joshnabbott", password: "josh_password"},
+      %Rumbl.User{id: "2", name: "Liz Abbott", username: "lizmabbott", password: "liz_password"},
+      %Rumbl.User{id: "3", name: "Griffin Abbott", username: "griftabbott", password: "griffin_password"}
+    ]
+  end
+
+  def all(_module), do: []
+
+  def get(module, id) do
+    Enum.find all(module), fn map -> map.id == id end
+  end
+
+  def get_by(module, params) do
+    Enum.find all(module), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end
+  end
+end
